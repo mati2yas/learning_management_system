19d4f137a5a6ccb02ac7c58e0b72a36c63be9f32
mati2yas <matiwaqseifu@gmailc.om>
2024-12-15 21:57:23 +0300

    fix : change on button type and alignment of interaction icon buttons

--- lib/core/common_widgets/course_card.dart
+++ lib/core/common_widgets/course_card.dart
@@ -18,150 +18,127 @@ class CourseCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var textTh = Theme.of(context).textTheme;
-    return Expanded(
-      child: Container(
-        width: double.infinity,
-        height: 160,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(
-            width: 1,
-            color: AppColors.mainGrey,
+    return Container(
+      width: double.infinity,
+      height: 180,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          width: 1,
+          color: AppColors.mainGrey,
+        ),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+            ),
+            child: Image.asset(
+              "assets/images/${course.image}",
+              height: 90,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
           ),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
-              ),
-              child: Image.asset(
-                "assets/images/${course.image}",
-                height: 90,
-                width: double.infinity,
-                fit: BoxFit.cover,
+          const SizedBox(height: 5),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              course.title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                color: AppColors.mainBlue,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
               ),
             ),
-            const SizedBox(height: 5),
-            Flexible(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Text(
-                  course.title,
-                  style: const TextStyle(
-                    color: AppColors.mainBlue,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: [
+                SvgPicture.asset(
+                  "assets/svgs/topics.svg",
+                  width: 12,
+                  height: 12,
+                  colorFilter: const ColorFilter.mode(
+                    AppColors.mainBlue,
+                    BlendMode.srcIn,
                   ),
                 ),
-              ),
+                const SizedBox(width: 4),
+                Text(
+                  "${course.topics} Topics",
+                  style: TextStyle(color: AppColors.darkerGrey),
+                ),
+              ],
             ),
-            Flexible(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Row(
+          ),
+          course.subscribed
+              ? Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    SvgPicture.asset(
-                      "assets/svgs/topics.svg",
-                      width: 12,
-                      height: 12,
-                      colorFilter: const ColorFilter.mode(
-                        AppColors.mainBlue,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                    const SizedBox(width: 4),
-                    Text("${course.topics} Topics"),
-                    const Spacer(),
-                  ],
-                ),
-              ),
-            ),
-            course.subscribed
-                ? Flexible(
-                    child: SizedBox(
-                      height: 35,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          TextButton.icon(
-                            onPressed: () {
-                              if (onLike != null) {
-                                onLike!();
-                              }
-                            },
-                            icon: Icon(
-                              course.liked
-                                  ? Icons.thumb_up
-                                  : Icons.thumb_up_outlined,
-                              color: AppColors.mainBlue,
-                            ),
-                            label: Text("${course.likes}"),
-                          ),
-                          TextButton.icon(
-                            style: TextButton.styleFrom(
-                                padding: const EdgeInsets.only(left: 8)),
-                            onPressed: () {
-                              if (onBookmark != null) {
-                                onBookmark!();
-                              }
-                            },
-                            icon: Icon(
-                              course.saved
-                                  ? Icons.bookmark
-                                  : Icons.bookmark_outline,
-                              color: AppColors.mainBlue,
-                            ),
-                            label: Text("${course.saves}"),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                : Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: FilledButton(
-                      style: FilledButton.styleFrom(
-                        backgroundColor: AppColors.mainBlue,
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 3,
-                          vertical: 2,
-                        ),
-                        foregroundColor: Colors.white,
-                        fixedSize: const Size(85, 15),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                            20,
-                          ),
-                        ),
-                      ),
+                    TextButton.icon(
                       onPressed: () {
-                        print("course subbed? ${course.subscribed}");
+                        if (onLike != null) {
+                          onLike!();
+                        }
                       },
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Buy",
-                            style: TextStyle(
-                              fontSize: 12,
-                            ),
-                          ),
-                          SizedBox(width: 8),
-                          Icon(
-                            Icons.lock,
-                            size: 14,
-                          ),
-                        ],
+                      icon: Icon(
+                        course.liked ? Icons.thumb_up : Icons.thumb_up_outlined,
+                        color: AppColors.mainBlue,
                       ),
+                      label: Text("${course.likes}"),
                     ),
+                    Spacer(),
+                    TextButton.icon(
+                      // style: TextButton.styleFrom(
+                      //     padding: const EdgeInsets.only(left: 8)),
+                      onPressed: () {
+                        if (onBookmark != null) {
+                          onBookmark!();
+                        }
+                      },
+                      icon: Icon(
+                        course.saved ? Icons.bookmark : Icons.bookmark_outline,
+                        color: AppColors.mainBlue,
+                      ),
+                      label: Text("${course.saves}"),
+                    ),
+                  ],
+                )
+              : Container(
+                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                  padding: EdgeInsets.symmetric(vertical: 6, horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: AppColors.mainBlue,
+                    borderRadius: BorderRadius.circular(16),
                   ),
-          ],
-        ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Buy",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Icon(
+                        Icons.lock,
+                        size: 14,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                )
+        ],
       ),
     );
   }
