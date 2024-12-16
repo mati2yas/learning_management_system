a5ec94ed5e3ef8f18bc9b32e1c4dd488467db293
Dechasa <Dechu373@protonmail.com>
2024-12-16 22:06:05 +0300

    commented out the screenshot disabling code

--- android/app/src/main/kotlin/com/example/learning_management_system/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/learning_management_system/MainActivity.kt
@@ -9,9 +9,14 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 import android.view.WindowManager.LayoutParams
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity()
+
+/*
+ {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         window.addFlags(LayoutParams.FLAG_SECURE)
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 } 
+
+ */
