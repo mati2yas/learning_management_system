31b0e45ee8edaf7129060f86d285f10196ce7d99
Dechasa <Dechu373@protonmail.com>
2025-02-18 15:11:29 +0300

    more fixes

--- lib/features/auth_login/data_source/login_data_source.dart
+++ lib/features/auth_login/data_source/login_data_source.dart
@@ -1,11 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lms_system/core/utils/db_service.dart';
 import 'package:lms_system/core/utils/dio_client.dart';
 import 'package:lms_system/core/utils/error_handling.dart';
 import 'package:lms_system/core/utils/storage_service.dart';
 import 'package:lms_system/features/shared/model/shared_user.dart';
+import 'package:path_provider/path_provider.dart';
 
 final loginDataSourceProvider = Provider<LoginDataSource>((ref) {
   return LoginDataSource(DioClient.instance, SecureStorageService());
@@ -35,11 +35,23 @@ class LoginDataSource {
         }
 
         final token = response.data["token"];
+        String avatar = response.data["data"]["user"]["avatar"];
+        avatar.replaceAll("\\", "");
+        avatar = "https://lms.biruklemma.com/$avatar";
+        debugPrint(avatar);
+
+        final directory = await getApplicationDocumentsDirectory();
+        final imageName = '${DateTime.now().millisecondsSinceEpoch}.jpg';
+        final savePath = '${directory.path}/$imageName';
+        await _dio.download(avatar, savePath);
+
         final user = User(
           id: response.data["data"]["user"]["id"],
           name: response.data["data"]["user"]["name"],
           email: email,
           password: password,
+          bio: response.data["data"]["user"]["bio"],
+          image: savePath,
           token: token,
         );
         debugPrint("token: $token");
