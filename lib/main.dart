2104665b173f066e6bc2f04fb57735dacc13bc32
mati2yas <matiwaqseifu@gmail.com>
2025-06-02 12:38:46 +0300

    fix : add system UI overlay to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lms_system/app/app.dart';
-import 'package:lms_system/core/constants/enums.dart';
-import 'package:lms_system/core/utils/root_checker.dart';
-import 'package:lms_system/features/auth_status_registration/provider/auth_status_controller.dart';
-import 'package:lms_system/features/check_seen_onboarding/provider/check_seen_onboarding_provider.dart';
-import 'package:lms_system/features/shared/model/start_routes.dart';
-import 'package:lms_system/features/shared/provider/start_routes_provider.dart';
-import 'package:lms_system/firebase_options.dart';
+import 'package:lms_system/core/constants/app_colors.dart';
 
-import 'core/app_router.dart';
+import 'package:lms_system/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: primaryColor, // Transparent status bar
+    statusBarIconBrightness: Brightness.light, // White icons
+    statusBarBrightness: Brightness.dark, // For iOS
+  ));
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
   runApp(
     const ProviderScope(child: MyApp()),
   );
 }
-
-
