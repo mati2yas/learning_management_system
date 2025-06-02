3aca00fa9a917c41ed39120244fa7da957c040b2
mati2yas <matiwaqseifu@gmail.com>
2025-06-02 13:02:10 +0300

    fix : implement dynamic onboarding screen title at all levels data model and data source

--- lib/features/onboarding/data_source/onboarding_data_source.dart
+++ lib/features/onboarding/data_source/onboarding_data_source.dart
@@ -5,15 +5,23 @@ class OnboardingDataSource {
     return [
       OnboardingModel(
         imagePath: 'assets/svgs/onboarding_page1.svg',
-        description: 'Gain vast knowledge from our excellent courses',
+        title: "Welcome to Excelet Academy — your all-in-one learning hub.",
+        description:
+            'From school subjects to real-world skills, we help you learn smarter, faster, and better.',
       ),
       OnboardingModel(
         imagePath: 'assets/svgs/onboarding_page2.svg',
-        description: 'Prepare For Exams With Our Large Collection of Exams',
+        title:
+            "Master any subject with expert-led lessons and engaging content.",
+        description:
+            "From elementary to university and beyond — explore a wide range of academic and professional courses, all in one place.",
       ),
       OnboardingModel(
         imagePath: 'assets/svgs/onboarding_page3.svg',
-        description: 'Achieve Your Study Goals With Tailored Content.',
+        title:
+            "Ace your exams with real past papers and detailed explanations.",
+        description:
+            "Practice for ESSLCE, Exit Exam, UAT, NGAT, Standardized Tests,Grade 6 & 8 Ministry exams, and more — with filters for better access. Explanations come in text, image, or video formats.",
       ),
     ];
   }
